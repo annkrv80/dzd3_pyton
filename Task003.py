c826ad9a071c8113ba7533cba2ddd2fa2cc6312f
Anna <annkrv80@yandex.ru>
2022-10-05 22:30:14 +0700

    Нахождение разницы между min и max дробной части числа

--- Task003.py
+++ Task003.py
@@ -2,12 +2,14 @@
 #которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
 import os
+import random
 import sys
 def clear(): return os.system('cls')
 
 
 clear()
-list = [7.3, 8.2, 1.01, 5, 6.4]
+list = [round(random.uniform(0.01, 20), 2) for x in range(6)]
+print(list)
 list2 = []
 for i in range(len(list)):
    c = round((list[i]-int(list[i])), 2)
