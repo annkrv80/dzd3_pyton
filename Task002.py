08d37118877ff66df5209577e2ee100ecfeaaafc
Anna <annkrv80@yandex.ru>
2022-10-19 22:12:14 +0700

    Найти произведение пар чисел списка

--- Task002.py
+++ Task002.py
@@ -7,10 +7,10 @@ def clear(): return os.system('cls')
 
 clear()
 
-list = [randint(-10, 10) for i in range(10)]
+list = [randint(-10, 10) for i in range(9)]
 print(list)
 list2 = []
-for i in range(len(list)//2):
+for i in range((len(list)+1)//2):#к длинне списка прибавили +1 чтобы центральный эл. умножить сам на себя
     box = list[i]*list[-i-1]
     list2.append(box)
 print(list2)
