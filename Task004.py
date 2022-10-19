6b0d66fdd7d41184e3460035d7b567e0cafbceaf
Anna <annkrv80@yandex.ru>
2022-10-19 22:49:54 +0700

    Перевод числа в двоичную систему

--- Task004.py
+++ Task004.py
@@ -12,3 +12,8 @@ while number > 0:
     numBin = str(number % 2)+numBin
     number = number//2
 print(numBin)
+
+n=256
+x=oct(n)#перевод в восмеричную систему
+print(x)
+print(int(x,8))#перевод обратно в десяятичную
