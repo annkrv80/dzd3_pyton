6ab35e3404c3e820a54255d79a0b7c7f8ea82677
Anna <annkrv80@yandex.ru>
2022-10-19 22:01:05 +0700

    Сумма элементов на нечетных позициях

--- Task011.py
+++ Task011.py
@@ -10,3 +10,6 @@ clear()
 list = [randint(0, 10) for i in range(5)]
 print(list)
 print(sum(list[1::2])) #срез списка от 1 до конца с шагом 2
+
+print(dir(list))#посмотреть доступные методы
+help(list.clear)#посмотреть что делает метод
